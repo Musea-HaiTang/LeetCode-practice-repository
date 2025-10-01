ccdacd1c825a51c14498425fc6f6cf7cff58799d
Musea-HaiTang <2875557129@qq.com>
2025-10-01 23:32:30 +0800

    create t5

---
+++ 数组/t5.py
@@ -0,0 +1,63 @@
+"""
+35. 搜索插入位置(简单)
+
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+"""
+
+# lower_bound 返回最小的满足 nums[i] >= target 的 i
+# 如果数组为空，或者所有数都 < target，则返回 len(nums)
+# 要求 nums 是非递减的，即 nums[i] <= nums[i + 1]
+
+# 闭区间写法
+def lower_bound(nums: list[int], target: int) -> int:
+    left, right = 0, len(nums) - 1  # 闭区间 [left, right]
+    while left <= right:  # 区间不为空
+        # 循环不变量：
+        # nums[left-1] < target
+        # nums[right+1] >= target
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1  # 范围缩小到 [mid+1, right]
+        else:
+            right = mid - 1  # 范围缩小到 [left, mid-1]
+    return left
+
+# 左闭右开区间写法
+def lower_bound2(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums)  # 左闭右开区间 [left, right)
+    while left < right:  # 区间不为空
+        # 循环不变量：
+        # nums[left-1] < target
+        # nums[right] >= target
+        mid = (left + right) // 2
+        if nums[mid] < target:
+            left = mid + 1  # 范围缩小到 [mid+1, right)
+        else:
+            right = mid  # 范围缩小到 [left, mid)
+    return left  # 或者 right
+
+# 开区间写法
+def lower_bound3(nums: list[int], target: int) -> int:
+    left, right = -1, len(nums)  # 开区间 (left, right)
+    while left + 1 < right:  # 区间不为空
+        mid = (left + right) // 2
+        # 循环不变量：
+        # nums[left] < target
+        # nums[right] >= target
+        if nums[mid] < target:
+            left = mid  # 范围缩小到 (mid, right)
+        else:
+            right = mid  # 范围缩小到 (left, mid)
+    return right
+
+class Solution:
+    @staticmethod
+    def searchInsert(nums: list[int], target: int) -> int:
+        return lower_bound(nums, target)  # 选择其中一种写法即可
+
+# 完全没想法，需要好好了解一下，不过这个是数组和二分查找的结合，得同时知道两个才行
+# 2025.10.1
