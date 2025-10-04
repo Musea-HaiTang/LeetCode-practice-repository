ee7612fa01d7b44c9ac5c7ef9ab6acfb7cb21d98
Musea-HaiTang <2875557129@qq.com>
2025-10-04 22:40:19 +0800

    create t3

---
+++ 编程基础从0到1/t3.py
@@ -0,0 +1,18 @@
+"""
+28. 找出字符串中第一个匹配项的下标(简单)
+给你两个字符串 haystack 和 needle。
+请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+如果 needle 不是 haystack 的一部分，则返回 -1。
+"""
+
+class Solution:
+    @staticmethod
+    def strStr(haystack: str, needle: str) -> int:
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
+                return i
+        return -1
+
+# 匹配字符串，这个还是很实用的
+# 还有更高效的方法：KMP算法
+# 2025.10.4
