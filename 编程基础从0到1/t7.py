02e4ed3eb7a558c597fe8715f0092c85fb3f79f0
Musea-HaiTang <2875557129@qq.com>
2025-10-09 23:12:10 +0800

    update t7

--- 编程基础从0到1/t7.py
+++ 编程基础从0到1/t7.py
@@ -1,12 +1,26 @@
 class Solution:
-    def arraySign(self, nums: list[int]) -> int:
-        nnum = 0
+    @staticmethod
+    def arraySign(nums: list[int]) -> int:
+        n_num = 0
         for i in nums:
             if i < 0:
-                nnum +=1
+                n_num +=1
+            elif i > 0:
+                continue
             else:
                 return 0
-        if nnum % 2 == 0:
+        if n_num % 2 == 0:
             return 1
         else:
             return -1
+
+
+    @staticmethod
+    def arraySign2(nums: list[int]) -> int:
+        sign = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            if num < 0:
+                sign = -sign
+        return sign
